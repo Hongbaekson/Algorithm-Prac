ff354eadcd4bf2b37e026cb31f9bb86ecdb15702
Hongbaekson <hbson1@myseneca.ca>
2021-07-04 22:25:13 +0900

    문자열 뒤집기 백9093번

---
+++ src/ReverseString.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class ReverseString {
+
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t  = Integer.parseInt(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		while(t --> 0) {
+				
+			String str =  br.readLine() + "\n";
+			
+			Stack<Character> s = new Stack<>();
+			for(char ch : str.toCharArray()) {
+				
+				if(ch =='\n'|| ch ==' ') {
+					while(!s.isEmpty()) {
+						bw.write(s.pop());
+						
+					}
+					bw.write(ch);
+			}else {
+				
+				s.push(ch);
+			}
+				
+				
+					
+		}
+			
+			
+		}
+		
+			bw.flush();
+	}
+
+}
